e0773136dfc64967c37e1277e8599aa615572636
Swaraj Dash <swarajd@mindfiresolutions.com>
2021-04-02 20:50:31 +0530

    Updates

---
+++ precache-manifest.8bf3a6a329bf28fe5cb13ffa978086c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de151ce69b9294e043b4b2af545bf40d",
+    "url": "/souvenir/index.html"
+  },
+  {
+    "revision": "58a6f36db530174b8ec2",
+    "url": "/souvenir/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/souvenir/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/souvenir/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58a6f36db530174b8ec2",
+    "url": "/souvenir/static/js/main.02ab44c7.chunk.js"
+  },
+  {
+    "revision": "75d67ff7e6449473e475",
+    "url": "/souvenir/static/js/runtime-main.aa5b3d50.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/souvenir/static/media/memories.5c0c63fb.png"
+  }
+]);
